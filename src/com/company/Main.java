76a4a030f716e588b502fe904229faf8637809d8
pmilecki <piotr.milecki96@gmail.com>
2022-03-24 14:41:30 +0100

    zadanie drugie i trzecie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,17 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
+        /*int[] array;
+        array = new int[]{4, 5, 7, 11, 12, 15, 15, 21, 40, 45};
+        System.out.println((third(array, 11)));*/
 
+    }
+
+    public static void first(){
         File file = new File("text.txt");
         int length = (int) file.length();
         try (FileInputStream stream = new FileInputStream(file);
-             InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
+             InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8)
         ){
             char[] data = new char[length];
             int readBytes = reader.read(data, 0, length);
@@ -26,8 +32,43 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void second(){
+        try(FileOutputStream fos = new FileOutputStream("text2.txt")) {
 
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
 
+            byte[] myText = new byte[0];
+            try {
+                String name = reader.readLine();
+                myText = name.getBytes();
+            } finally {
+                fos.write(myText);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static double third(int[] array, int value){
+        int index = 0;
+        int limit = array.length - 1;
+
+        while (index <= limit){
+            var point = Math.ceil((index + limit)/2);
+            var entry = array[(int) point];
+            if (value < point){
+                index = (int) (point + 1);
+                continue;
+            }
+            if (value < entry){
+                limit = (int) (point - 1);
+                continue;
+            }
+            return point;
+        }
+        return -1;
     }
 }
