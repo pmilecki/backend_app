f78068bb4490ddd2fdc1050e7d115517ac9942ad
pmilecki <51510740+pmilecki@users.noreply.github.com>
2022-04-08 19:38:17 +0200

    lab_02 (zadania 1-4)

---
+++ lab_02/demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String HelloWorld() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/example")
+    public String index() {
+        return "index";
+    }
+
+}
+
